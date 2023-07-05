7cd518d7c67968e133fb68fc56febaaac4152f9f
JoseIgnacioTapia <josignaciot@gmail.com>
2023-07-05 18:46:58 -0300

    Logic for managing to-do tasks

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import colors from "colors";
 
 import { inquireMenu, pausa } from "./helpers/inquirer.js";
+import Tarea from "./models/tarea.js";
+import Tareas from "./models/tareas.js";
 
 console.clear();
 
@@ -10,8 +12,14 @@ const main = async () => {
   let opt = "";
 
   do {
-    opt = await inquireMenu();
-    console.log({ opt });
+    // opt = await inquireMenu();
+    // console.log({ opt });
+
+    const tareas = new Tareas();
+    const tarea = new Tarea("Comprar comida");
+
+    tareas._listado[tarea.id] = tarea;
+    console.log(tareas);
 
     await pausa();
   } while (opt !== "0");
---
+++ models/tareas.js
@@ -0,0 +1,14 @@
+/**
+ *   _listado:
+ *      { 'uuid-12345-1233-2: {id:12, desc:asd,completadoEN:92231} }
+ */
+
+class Tareas {
+  _listado = {};
+
+  constructor() {
+    this._listado = {};
+  }
+}
+
+export default Tareas;
